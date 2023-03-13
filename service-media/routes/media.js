32eb312e212b6925cbede1e7e6fd6ace97840293
yoga-saputra <nyogasaputra_pro@alphaphilindo.com>
2023-03-13 14:23:50 +0800

    service media

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getMedia, createMedia, deleteMedia } = require('../handler/handler')
 
-// get media
+// media route
 router.get('/', getMedia)
-// create media
 router.post('/', createMedia)
-
 router.delete('/:id', deleteMedia)
 
 
